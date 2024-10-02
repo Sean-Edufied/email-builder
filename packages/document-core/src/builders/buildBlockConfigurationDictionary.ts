b830d7677514b31c2d130969eb635323d4c68253
taneron <git@taner-dev.com>
2024-10-02 09:01:22 +0200

    further fixing

--- packages/document-core/src/builders/buildBlockConfigurationDictionary.ts
+++ packages/document-core/src/builders/buildBlockConfigurationDictionary.ts
@@ -1,12 +1,14 @@
-import { BaseZodDictionary, DocumentBlocksDictionary } from '../utils';
+import { BaseZodDictionary, DocumentBlocksDictionary } from "../utils";
 
 /**
  * Identity function to type a DocumentBlocksDictionary
  * @param blocks Main DocumentBlocksDictionary
  * @returns typed DocumentBlocksDictionary
  */
-export default function buildBlockConfigurationDictionary<T extends BaseZodDictionary>(
-  blocks: DocumentBlocksDictionary<T>
+export default function buildBlockConfigurationDictionary<
+  T extends BaseZodDictionary,
+>(
+  blocks: DocumentBlocksDictionary<T>,
 ) {
   return blocks;
 }
